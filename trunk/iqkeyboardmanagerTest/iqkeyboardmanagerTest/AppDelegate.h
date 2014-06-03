4a08a98667aa5150448d247f55c93b95f46ed7c0
jession <jession@apples-MacBook-Pro.local>
2014-06-03 09:42:24 +0800

    first commit

---
+++ trunk/iqkeyboardmanagerTest/iqkeyboardmanagerTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iqkeyboardmanagerTest
+//
+//  Created by jession on 14-6-2.
+//  Copyright (c) 2014年 we-win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ trunk/iqkeyboardmanagerTest/iqkeyboardmanagerTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iqkeyboardmanagerTest
+//
+//  Created by jession on 14-6-2.
+//  Copyright (c) 2014年 we-win. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
